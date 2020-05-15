524fba433facb9bbec36b6f11fa5e911b1695433
Nolan Picini <nolanpic@gmail.com>
2020-05-14 18:47:08 -0700

    RingBuffer passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,38 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = []
+        self.oldest_index = None
 
     def append(self, item):
-        pass
+        # what if the queue is UNDER capacity?
+        if len(self.queue) < self.capacity:
+            # if this is the first item to be added,
+            # set oldest to this item if it's the first
+            if len(self.queue) == 0:
+                self.oldest_index = 0
+            else:
+                self.oldest_index +=1
+            # append the item
+            self.queue.append(item)
+            
+        # what if the queue is AT capacity?
+        else:
+            # update the oldest index
+            
+            # if the index was at the last item in
+            # the array, move index to the front
+            if self.oldest_index == self.capacity-1:
+                self.oldest_index = 0
+            # otherwise, just increment the oldest
+            # index to the next item to the right
+            else:
+                self.oldest_index +=1
+                
+            # replace item at oldest index
+            self.queue[self.oldest_index] = item
+            
 
     def get(self):
-        pass
+        return self.queue
+    
